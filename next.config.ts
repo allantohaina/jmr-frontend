3a802efb73cf64297610f2b635f9c6be65c798e8
allan <allantohaina2@gmail.com>
2026-08-06 16:42:55 +0300

    chore(next): limit build to 1 cpu + disable worker threads (memory on hala)

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
 };
 
 export default nextConfig;
