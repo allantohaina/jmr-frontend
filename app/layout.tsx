988cbc2664f975ef7df0b44e13bfa2131842feef
allan <allantohaina2@gmail.com>
2026-08-07 20:38:20 +0300

    feat: replace favicons with new set + site.webmanifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
-      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
-      { url: "/favicon-48x48.png", type: "image/png", sizes: "48x48" },
-      { url: "/favicon-192x192.png", type: "image/png", sizes: "192x192" },
-      { url: "/favicon-512x512.png", type: "image/png", sizes: "512x512" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/web-app-manifest-192x192.png", type: "image/png", sizes: "192x192" },
+      { url: "/web-app-manifest-512x512.png", type: "image/png", sizes: "512x512" },
     ],
     shortcut: ["/favicon.ico"],
     apple: [{ url: "/apple-touch-icon.png", type: "image/png", sizes: "180x180" }],
