d9a14ea227dd09d170e93df3343ad977fa127e8a
allan <allantohaina2@gmail.com>
2026-08-11 12:31:20 +0300

    Fix: remove prerender/dynamic incompatible with static export

--- app/demande-devis/page.tsx
+++ app/demande-devis/page.tsx
@@ -2,9 +2,6 @@
 
 import { DemandeDevisSection } from "@/app/components";
 
-export const dynamic = 'force-dynamic';
-export const prerender = false;
-
 export default function DemandeDevisPage() {
   return <DemandeDevisSection />;
 }
